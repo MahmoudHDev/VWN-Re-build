5c80cb0c3c4fc213b845df9ad7cd72e18a60fa43
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2022-08-05 14:58:37 +0200

    add Files

---
+++ NVMProject-Re-build/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NVMProject-Re-build
+//
+//  Created by Mahmoud on 8/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
